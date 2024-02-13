b84f49e195f7eab9eb325a107bc95bf25b7b3b90
a-dabour <ah910905@gmail.com>
2024-02-13 21:17:31 +0200

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Auth class for handling authentication with the API."""
+from flask import request
+from typing import List, TypeVar
+# from models.user import User
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """authenticate"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """auth header"""
+        return None
+
+    User = TypeVar('User')
+
+    def current_user(self, request=None) -> User:
+        """current user"""
+        return None
