344f1bfb60882cec52dd9e9b7088f10530b08182
a-dabour <ah910905@gmail.com>
2024-02-14 20:08:25 +0200

    handling require_path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,16 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
-        """authenticate"""
-        return False
+        """needs authentication or not"""
+        if path is None or excluded_paths is None \
+                or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for pth in excluded_paths:
+            if pth == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """auth header"""
