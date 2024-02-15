d844dc4c05034b626f5231dd6f3d05f1167479f5
a-dabour <ah910905@gmail.com>
2024-02-15 19:00:47 +0200

    finding instances of a class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,9 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models import user
+User = TypeVar('User')
 
 
 class BasicAuth(Auth):
@@ -40,3 +42,18 @@ class BasicAuth(Auth):
                 or ':' not in decoded:
             return (None, None)
         return tuple(decoded.split(':', 1))
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd:
+                                     str) -> user:
+        """returns user object for credentials if present"""
+        if user_email is None or type(user_email) is not str or \
+                user_pwd is None or type(user_pwd) is not str:
+            return None
+        # check if user exists
+        result = user.User.search({'email': user_email})
+        if result:
+            for u in result:
+                if u.is_valid_password(user_pwd):
+                    return u
+        return None
