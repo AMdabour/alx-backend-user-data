e9f7993ba0dc8e0a21465dd83aac16980052556b
a-dabour <ah910905@gmail.com>
2024-02-17 09:15:03 +0200

    updating before request method

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,11 +30,12 @@ def before_request():
     if auth is None:
         return
     exc_pths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     request.current_user = auth.current_user(request)
     if auth.require_auth(request.path, exc_pths) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
