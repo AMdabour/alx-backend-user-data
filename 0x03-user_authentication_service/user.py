1ad5ae086f50fe2b740d3fc250ed8e01f51e2bf7
a-dabour <ah910905@gmail.com>
2024-02-21 17:44:47 +0200

    edit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 """creating the User model using SQLAlchemy"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
